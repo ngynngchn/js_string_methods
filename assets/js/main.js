45d7d2cc564e6dd4736a01f460fe06d2ad876e9c
Hanni <ngynngchn@gmail.com>
2023-01-03 14:30:37 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let x = "Hello ";
+let y = "World<br>";
+
+let z = x + y;
+
+document.write(z);
+
+let n = x + " " + y;
+
+document.write(n);
+
+x += "World<br>";
+
+document.write(x);
+
+let meinString = "Ich bin Erster";
+
+meinString += " Ich komme auf Platz zwei";
+
+document.write(meinString);
